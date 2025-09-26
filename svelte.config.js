ce11a2a8d982bf315a3932e60ebede04a2e544f2
kimtaeryong78 <ktl3484@korea.ac.kr>
2025-09-26 23:41:10 +0900

    version issue

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      		runtime: 'nodejs18.x' // Node 16 또는 18로 빌드하도록 지정
+    	}),
 		alias: {
 			$db: './src/db'
 		}
